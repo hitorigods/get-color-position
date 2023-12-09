9e67fb35c5ab1e2266b56f8838366d51bb06d076
hitorigods <hitorigods1124@gmail.com>
2023-12-09 18:11:23 +0900

    インデントをタブに変更

--- main.py
+++ main.py
@@ -8,48 +8,48 @@ import json
 
 # 設定ファイルを読み込む関数
 def load_config(file_path):
-    with open(file_path, 'r', encoding='utf-8', errors='replace') as f:
-        return yaml.safe_load(f)
+	with open(file_path, 'r', encoding='utf-8', errors='replace') as f:
+		return yaml.safe_load(f)
 
 # CSVファイルを作成する関数
 def create_csv_file(file_path):
-    with open(file_path, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['scene', 'ext', 'target', 'x', 'y'])
+	with open(file_path, 'w', newline='') as f:
+		writer = csv.writer(f)
+		writer.writerow(['scene', 'extension', 'target', 'x', 'y'])
 
 # JSONデータに情報を追加する関数
 def add_to_json_data(data, img_path, color, cx, cy):
-    data.append({
-        'scene': os.path.basename(img_path).split('.')[0],
-        'ext': os.path.splitext(img_path)[1][1:],
-        'target': color,
-        'x': cx,
-        'y': cy
-    })
+	data.append({
+		'scene': os.path.basename(img_path).split('.')[0],
+		'extension': os.path.splitext(img_path)[1][1:],
+		'target': color,
+		'x': cx,
+		'y': cy
+	})
 
 # JSONファイルに書き込む関数
 def write_to_json_file(file_path, data):
-    with open(file_path, 'w') as f:
-        json.dump(data, f, indent=4)
+	with open(file_path, 'w') as f:
+		json.dump(data, f, indent=4)
 
 # 画像を処理する関数
 def process_images(input_path, output_csv, output_json, colors):
-    json_data = []
-    for img_path in glob.glob(os.path.join(input_path, '*')):
-        img = cv2.imread(img_path)
-        for color, value in colors.items():
-            lower = np.array(value, dtype=np.uint8)
-            upper = np.array(value, dtype=np.uint8)
-            mask = cv2.inRange(img, lower, upper)
-            moments = cv2.moments(mask)
-            if moments['m00'] != 0:
-                cx = int(moments['m10'] / moments['m00'])
-                cy = int(moments['m01'] / moments['m00'])
-                with open(output_csv, 'a', newline='') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([os.path.basename(img_path).split('.')[0], os.path.splitext(img_path)[1][1:], color, cx, cy])
-                add_to_json_data(json_data, img_path, color, cx, cy)
-    write_to_json_file(output_json, json_data)
+	json_data = []
+	for img_path in glob.glob(os.path.join(input_path, '*')):
+		img = cv2.imread(img_path)
+		for color, value in colors.items():
+			lower = np.array(value, dtype=np.uint8)
+			upper = np.array(value, dtype=np.uint8)
+			mask = cv2.inRange(img, lower, upper)
+			moments = cv2.moments(mask)
+			if moments['m00'] != 0:
+				cx = int(moments['m10'] / moments['m00'])
+				cy = int(moments['m01'] / moments['m00'])
+				with open(output_csv, 'a', newline='') as f:
+					writer = csv.writer(f)
+					writer.writerow([os.path.basename(img_path).split('.')[0], os.path.splitext(img_path)[1][1:], color, cx, cy])
+				add_to_json_data(json_data, img_path, color, cx, cy)
+	write_to_json_file(output_json, json_data)
 
 # 設定ファイルを読み込む
 config = load_config('config.yaml')
